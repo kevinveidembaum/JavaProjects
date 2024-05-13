aa084a29de4fd60843bd7263dd220220c0fb84bc
Kevin Nikolai <117945004+kevinveidembaum@users.noreply.github.com>
2024-05-13 15:22:39 -0300

    method getter and setter

---
+++ SpecialsMethods/src/specialsmethods/Pen.java
@@ -0,0 +1,54 @@
+package specialsmethods;
+
+public class Pen {
+    private String modelo, cor;
+    private float ponta;
+    private boolean tampada;
+
+    public Pen(String modelo, String cor, float ponta) {
+        this.modelo = modelo;
+        this.cor = cor;
+        this.ponta = ponta;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public float getPonta() {
+        return ponta;
+    }
+
+    public void setPonta(float ponta) {
+        this.ponta = ponta;
+    }
+
+    public boolean isTampada() {
+        return tampada;
+    }
+
+    public void setTampada(boolean tampada) {
+        this.tampada = tampada;
+    }
+    
+    public void status(){
+        System.out.println("Caracteristicas da Caneta: ");
+        System.out.println("Modelo: " + this.getModelo());
+        System.out.println("Ponta: " + this.getPonta());
+        System.out.println("Cor: "+ this.getCor());
+        System.out.println("Está tampada? "+ this.isTampada());
+    }
+    
+}
---
+++ SpecialsMethods/src/specialsmethods/SpecialsMethods.java
@@ -0,0 +1,11 @@
+package specialsmethods;
+
+public class SpecialsMethods {
+    public static void main(String[] args) {
+        Pen caneta = new Pen("BIC", "Azul", 0.3f);
+        caneta.status();
+        Pen caneta2 = new Pen("Faber Castel", "Vermelha", 1.5f);
+        caneta2.status();
+    }
+    
+}
